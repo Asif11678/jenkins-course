919c5de3d91379a63814b96c07da3c392ec5a5b3
Asif11678 <39298164+Asif11678@users.noreply.github.com>
2021-08-19 00:32:33 +0530

    updated docker hub repo

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('asifkhan1018/nodejsdemo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
